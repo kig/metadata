de640bec792155023e75b5071e7e586a96623c41
Ilmari Heikkinen <kig@manifold.fhtr.org>
2007-09-15 09:57:44 +0300

    mkrelease.rb: add missing }

--- mkrelease.rb
+++ mkrelease.rb
@@ -5,16 +5,16 @@ require 'fileutils'
 
 dn = "metadata-#{Metadata::VERSION}"
 FileUtils.mkdir(dn)
-%(lib bin README INSTALL).each{|fn|
+%w(lib bin README INSTALL).each{|fn|
   FileUtils.cp_r(fn, dn)
 }
 `tar zcf #{dn}.tar.gz #{dn}`
 FileUtils.rm_r(dn)
-s = "
+s = <<-EOF
 tarball: http://dark.fhtr.org/repos/metadata/#{dn}.tar.gz
 git: http://dark.fhtr.org/repos/metadata
 
 
-#{File.read('README')
-"
+#{File.read('README')}
+EOF
 File.open("release_msg-#{Metadata::VERSION}.txt", "w"){|f| f.write s}
